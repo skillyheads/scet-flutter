192ba15ec2e94f08a4d0cc0b6d2399e2f088d454
Skillyheads <code@skillyheads.com>
2025-06-23 12:38:37 +0530

    Initial Commit

---
+++ dartpost/bin/dartpost.dart
@@ -0,0 +1,5 @@
+import 'package:dartpost/dartpost.dart' as dartpost;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dartpost.calculate()}!');
+}
---
+++ dartpost/test/dartpost_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartpost/dartpost.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
